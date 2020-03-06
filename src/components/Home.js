b29b5e14201c753421daba0cb7b3d060994645b4
Prakasha, Prashanth <prashanth.prakasha@philips.com>
2020-03-06 11:18:59 +0530

    c

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Extensions from './Extensions'
 
 import qs from 'qs';
 
@@ -14,7 +13,6 @@ const Home = () => {
   const [code, setCode] = useState('');
   const [accessToken, setAccessToken] = useState('');
   const [resource, setResource] = useState([]);
-  const [listURLs, setListURLs] = useState([])
 
   const parm1 = 'response_type=code';
   const parm2 = 'client_id=id123';
@@ -64,8 +62,7 @@ const Home = () => {
       return (
         <div>
           <h4>Authorization accepted, Below is the CODE</h4>
-          <textarea cols="150" rows="5">
-            {code}
+          <textarea cols="150" rows="5" defaultValue={code}>
           </textarea>
         </div>
       );
@@ -83,8 +80,7 @@ const Home = () => {
           </button>
           <br></br>
           <br></br>
-          <textarea cols="150" rows="15">
-            {accessToken}
+          <textarea cols="150" rows="15" defaultValue={accessToken}>
           </textarea>
         </div>
       );
@@ -94,111 +90,123 @@ const Home = () => {
   };
 
   const RESOURCE = () => {
-    var listURLs = []
     if (accessToken) {
-      axios({
-        method: 'get',
-        url: `${localStorage.getItem('fhirUrl')}/Patient`,
-        headers: { Authorization: `Bearer ${accessToken}` }
-      })
-        .then(resp => {
-          setResource(resp.data.entry)
-          listURLs = resource.map((items) => <li>{items}</li>)          
-        })
-        .catch(errfinal => { });
-      return listURLs
+      return (
+        <div>
+          <button onClick={handleRESclick}>
+            Fetch Observation Resource
+          </button>
+          <br></br>
+          <br></br>
+          {/* <textarea cols="150" rows="15" defaultValue={resource}> */}
+          {resource}
+          {/* </textarea> */}
+        </div>
+      );
     } else {
-      return 'waiting for access_token';
+      return 'waiting for access token';
     }
-  }
 
-  const handlePOSTclick = () => {
-    async function handlePOST() {
-      const requestBody = {
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: 'http://localhost:3000/Home',
-        client_id: 'app-client-id'
-      };
-      const config = {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      };
-      let posturl = localStorage.getItem('tokenUrl');
-      const result = await axios
-        .post(posturl, qs.stringify(requestBody), config)
-        .then(resp => {
-          setAccessToken(resp.data.access_token);
+    const handleRESclick = () => {
+      async function RESclick() {
+        axios({
+          method: 'get',
+          url: `${localStorage.getItem('fhirUrl')}/Observation`,
+          headers: { Authorization: `Bearer ${accessToken}` }
         })
-        .catch(err => {
-          setAccessToken(err + '\n Authorization CODE might have expired');
-        });
+          .then(resp => {
+            const rs = resp.data.entry.map(i => <li key={i.fullUlr}>{i.fullUrl}</li>)
+            console.log(rs)
+            setResource(rs)
+            // const rs = resource.map(i => i.fullUrl)
+            // console.log(rs)
+            // listURLs = resource.map((items) => <li>{items}</li>)          
+          })
+          .catch();
+        return resource
+      }
+      RESclick();
     }
-    handlePOST();
+
+    const handlePOSTclick = () => {
+      async function handlePOST() {
+        const requestBody = {
+          grant_type: 'authorization_code',
+          code: code,
+          redirect_uri: 'http://localhost:3000/Home',
+          client_id: 'app-client-id'
+        };
+        const config = {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        };
+        let posturl = localStorage.getItem('tokenUrl');
+        const result = await axios
+          .post(posturl, qs.stringify(requestBody), config)
+          .then(resp => {
+            setAccessToken(resp.data.access_token);
+          })
+          .catch(err => {
+            setAccessToken(err + '\n Authorization CODE might have expired');
+          });
+      }
+      handlePOST();
+    };
+
+    async function handleFETCHclick() {
+      try {
+        const response = await fetch(`${fhirUrl}/metadata`, {
+          headers: { Accept: 'application/json' }
+        });
+        const json = await response.json();
+
+        setAuthUrl(json.rest[0].security.extension[0].extension[0].valueUri);
+        localStorage.setItem(
+          'authUrl',
+          json.rest[0].security.extension[0].extension[0].valueUri
+        );
+
+        setTokenUrl(json.rest[0].security.extension[0].extension[1].valueUri);
+        localStorage.setItem(
+          'tokenUrl',
+          json.rest[0].security.extension[0].extension[1].valueUri
+        );
+      } catch (error) { }
+    }
+
+    const handleTEXTchange = e => {
+      setFhirUrl(e.target.value);
+      localStorage.setItem('fhirUrl', e.target.value);
+    };
+
+    return (
+      <div>
+        <form>
+          <input type="text" onChange={handleTEXTchange} />
+          <input type="button" value="SMART Fetch" onClick={handleFETCHclick} />
+          <br></br>
+          <br></br>
+        </form>
+        <hr></hr>
+        <ENDPOINT></ENDPOINT>
+        <AUTHURL></AUTHURL>
+        <TOKENURL></TOKENURL>
+
+        <h3>Step-1: App asks for authorization, asking for end-user input</h3>
+        <LOGINURL></LOGINURL>
+        <hr></hr>
+
+        <h3>Step-2: EHR evaluates authorization request and provides CODE</h3>
+        <CODE></CODE>
+        <hr></hr>
+
+        <h3>Step-3: App exchanges authorization code for access token</h3>
+        <POST></POST>
+        <hr></hr>
+
+        <h3>Step 4: App accesses clinical data via FHIR API</h3>
+        <RESOURCE></RESOURCE>
+      </div>
+    );
   };
 
-  async function handleFETCHclick() {
-    try {
-      const response = await fetch(`${fhirUrl}/metadata`, {
-        headers: { Accept: 'application/json' }
-      });
-      const json = await response.json();
-
-      setAuthUrl(json.rest[0].security.extension[0].extension[0].valueUri);
-      localStorage.setItem(
-        'authUrl',
-        json.rest[0].security.extension[0].extension[0].valueUri
-      );
-
-      setTokenUrl(json.rest[0].security.extension[0].extension[1].valueUri);
-      localStorage.setItem(
-        'tokenUrl',
-        json.rest[0].security.extension[0].extension[1].valueUri
-      );
-    } catch (error) { }
-  }
-
-  const handleTEXTchange = e => {
-    setFhirUrl(e.target.value);
-    localStorage.setItem('fhirUrl', e.target.value);
-  };
-
-  const handleCLEARclick = () => {
-    localStorage.clear();
-  };
-
-  return (
-    <div>
-      <form>
-        <input type="text" onChange={handleTEXTchange} />
-        <input type="button" value="SMART Fetch" onClick={handleFETCHclick} />
-        <input type="button" value="CLEAR URL's" onClick={handleCLEARclick} />
-        <br></br>
-        <br></br>
-      </form>
-      <hr></hr>
-      <ENDPOINT></ENDPOINT>
-      <AUTHURL></AUTHURL>
-      <TOKENURL></TOKENURL>
-
-      <Extensions authUR='testauth'></Extensions>
-      <hr></hr>
-
-      <h3>Step-1: App asks for authorization, asking for end-user input</h3>
-      <LOGINURL></LOGINURL>
-      <hr></hr>
-
-      <h3>Step-2: EHR evaluates authorization request and provides CODE</h3>
-      <CODE></CODE>
-      <hr></hr>
-
-      <h3>Step-3: App exchanges authorization code for access token</h3>
-      <POST></POST>
-      <hr></hr>
-
-      <h3>Step 4: App accesses clinical data via FHIR API</h3>
-      <RESOURCE></RESOURCE>
-    </div>
-  );
-};
-
-export default Home;
+  export default Home;
